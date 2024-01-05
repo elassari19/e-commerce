cf9353e7ca8106d2612aac0edf272fb9756b6d42
elassari19 <elassari19@gmail.com>
2024-01-05 20:02:30 +0100

    clear bugs

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -1,16 +1,12 @@
 "use client"
 
 import React from 'react'
-import { ColStack, RowStack } from '../layout'
 import { Button } from '../ui/button'
-import Social from './Social'
 import { useSelector } from 'react-redux'
 import { Formik, Form } from 'formik';
 import FormikField from '../inputs/FormikField'
 import { signinSchema, signupType } from '../../schema'
-import SelectCountry from '../Search/SelectCountry'
 import { postMethod } from '@/helpers/restApiMethods'
-import { useToast } from '../ui/use-toast'
 import { RootState } from '../../store'
 import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
@@ -20,25 +16,16 @@ interface Props  extends React.HtmlHTMLAttributes<HTMLDivElement> {}
 
 const Register = ({  }: Props) => {
   const country = useSelector((state: RootState) => state.root.country)
-  const { toast } = useToast()
   const router = useRouter()
   const { data } = useSession()
-
-  if(data?.user?.email) {
-    router.push("/")
-    return <Typography heading='h2' size="2xl" font="bold" center>already sign in</Typography>
-  }
 
   const onSubmit = async (values: signupType) => {
     const res = await postMethod("/api/register", values);
     // console.log(res.status)
     if(res.status === 201) {
-      toast({ variant: "success", title: 'success', description: `create ${values.email} account successeeded` })
-      router.refresh()
       router.push('/sign-in')
       return;
     }
-    toast({ variant: "destructive", title: 'Failed', description: `create ${values.email} account Faileded` })
   };
 
   return (
@@ -55,39 +42,7 @@ const Register = ({  }: Props) => {
         formik => {
           return (
             <Form>
-              {/* form section */}
-              <ColStack className='gap-4'>
-
-                <SelectCountry name="code" />
-
-                <FormikField
-                  name='email'
-                  placeholder='Email'
-                />
-                <FormikField
-                  type="password"
-                  name='password'
-                  placeholder='Password'
-                />
-
-                <Button
-                  type="submit"
-                  variant="secondary"
-                  size="lg"
-                  className='rounded-full'
-                  disabled={!formik.isValid && formik.isSubmitting}
-                >Create account</Button>
-
-              {/* divider section */}
-              <RowStack className='gap-8 my-4'>
-                <hr className='flex-1'/>
-                <span>Or continue with</span>
-                <hr className='flex-1'/>
-              </RowStack>
-
-              {/* social section */}
-              <Social confirm />
-              </ColStack>
+              
             </Form>
           )
         }
