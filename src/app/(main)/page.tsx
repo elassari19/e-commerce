4f3c622f94a861b739268370de9d41afab2bf6fa
elassari19 <elassari19@gmail.com>
2024-02-21 23:00:48 +0100

    try

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,5 @@
-import HeaderNav from "@/components/nav/HeaderNav"
 
 interface Props {
-  searchParams: {
-    search?: string
-    q?: string
-  }
 }
 
 export default async function Home({  }: Props) {
@@ -12,7 +7,7 @@ export default async function Home({  }: Props) {
   return (
     <div className="grid grid-cols-12">
       <div className="col-span-full bg-primary-foreground text-white">
-        <HeaderNav />
+        {/* <HeaderNav /> */}
       </div>
     </div>
   )
