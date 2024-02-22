e47849d37622a5b38f5d789f2c5cb3be0276936e
elassari19 <elassari19@gmail.com>
2024-02-22 22:12:13 +0100

    fix vercel error

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,7 @@
 import HeaderNav from "@/components/nav/HeaderNav"
 import SearchProductsNav from "../../components/nav/SearchProductsNav"
 import { db } from "../../lib/db"
+import { ImageUrl, Product } from "@prisma/client"
 
 interface Props {
   searchParams: {
@@ -10,18 +11,24 @@ interface Props {
 }
 
 export default async function Home({ searchParams }: Props) {
-  const products = await db.product.findMany({
-    where: {
-      OR: [
-        { name: { contains: searchParams?.q, mode: "insensitive" } },
-        { description: { contains: searchParams?.q, mode: "insensitive" } }
-      ]
-    },
-    include: {
-      images: true
-    }
-  })
+  let products: Partial<Product& { images: ImageUrl[]}>[] = []
 
+  try {
+    products = await db.product.findMany({
+      where: {
+        OR: [
+          { name: { contains: searchParams?.q, mode: "insensitive" } },
+          { description: { contains: searchParams?.q, mode: "insensitive" } }
+        ]
+      },
+      include: {
+        images: true
+      }
+    })
+  } catch (error) {
+    products = []
+    console.error('Error fetching products', error)
+  }
   return (
     <div className="grid grid-cols-12">
       <div className="col-span-full bg-primary-foreground text-white">
