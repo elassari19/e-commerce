0e382deb5068012d15b22c2003462724631f538e
elassari19 <elassari19@gmail.com>
2024-10-22 21:35:30 +0100

    change brand icon

--- src/components/atoms/Brand.tsx
+++ src/components/atoms/Brand.tsx
@@ -1,24 +1,41 @@
-import { cn } from "@/lib/utils"
-import { RowStack } from "../layout"
-import Typography from "../layout/typography"
-import { motion } from "framer-motion"
-import image from "@/public/logo.png"
-import Image from "next/image"
+import { cn } from '@/lib/utils';
+import { RowStack } from '../layout';
+import Typography from '../layout/typography';
+import { motion } from 'framer-motion';
+import logo from '@/assets/logo.png';
+import Image from 'next/image';
 
-interface Props  extends React.HtmlHTMLAttributes<HTMLDivElement> {
-  nav?: boolean
+interface Props extends React.HtmlHTMLAttributes<HTMLDivElement> {
+  nav?: boolean;
 }
 
-const Brand = ({ className, nav=true }: Props) => {
-  const logo = "https://res.cloudinary.com/elassari/image/upload/v1711499475/my-ecom-app/assets/vcwujmu0yvc9k4bc1adw.png"
+const Brand = ({ className, nav = true }: Props) => {
+  // const logo = "https://res.cloudinary.com/elassari/image/upload/v1711499475/my-ecom-app/assets/vcwujmu0yvc9k4bc1adw.png"
 
-return <RowStack className={cn("gap-2 px-4 py-2 justify-center", className)}>
-    {/* <Store size={28} fill="#0f0" color="#0004" /> */}
-    <Image src={logo} alt="shop logo"  loading="eager" width={50} height={50} className="h-10 w-10" />
-    <motion.div layout className={`${!nav && "hidden"} transition-transform duration-500`}>
-      <Typography heading="h1" className="text-xl font-bold text-primary whitespace-nowrap">ByYan Store</Typography> 
-    </motion.div>
-  </RowStack>
-}
+  return (
+    <RowStack className={cn('gap-2 px-4 py-2 justify-center', className)}>
+      {/* <Store size={28} fill="#0f0" color="#0004" /> */}
+      <Image
+        src={logo}
+        alt="shop logo"
+        loading="eager"
+        width={50}
+        height={50}
+        className="h-10 w-10"
+      />
+      <motion.div
+        layout
+        className={`${!nav && 'hidden'} transition-transform duration-500`}
+      >
+        <Typography
+          heading="h1"
+          className="text-xl font-bold text-primary whitespace-nowrap"
+        >
+          Store
+        </Typography>
+      </motion.div>
+    </RowStack>
+  );
+};
 
-export default Brand
+export default Brand;
--- src/components/nav/HeaderNav.tsx
+++ src/components/nav/HeaderNav.tsx
@@ -11,27 +11,32 @@ import BasketCard from '../cards/BasketCard';
 import { CartBadge } from '../reduxtHandler/CartActions';
 import DialogPopup from '../DialogPopup';
 import Signin from '../forms/Signin';
-import { db } from '../../lib/db';
-import AllCategories from './AllCategories';
 import { list } from '../../helpers/constants/Categories';
 import UserAuth from '../auth/UserAuth';
 import { getRootCategories } from '../../helpers/actions/categories';
+import logo from '@/assets/logo.png';
 
-interface Props {
-}
-const logo = "https://res.cloudinary.com/elassari/image/upload/v1711499475/my-ecom-app/assets/vcwujmu0yvc9k4bc1adw.png"
+interface Props {}
+// const logo = "https://res.cloudinary.com/elassari/image/upload/v1711499475/my-ecom-app/assets/vcwujmu0yvc9k4bc1adw.png"
 
-const HeaderNav = async ({  }: Props) => {
-  const session = await getAuthSession()
-  const Categories = await getRootCategories()
+const HeaderNav = async ({}: Props) => {
+  const session = await getAuthSession();
+  const Categories = await getRootCategories();
 
   return (
     <header className="relative mx-2 md:container md:mx-auto pt-2 pb-2">
       <MotionSlide top={10}>
         <nav className="flex justify-between items-center gap-8 md:gap-24">
-          <div className='cursor-pointer'>
+          <div className="cursor-pointer rounded-full bg-primary-foreground">
             <Link href="/">
-              <Image src={logo} loading="eager" alt='app brand' width={100} height={50}  className='h-10 w-10' />
+              <Image
+                src={logo}
+                loading="eager"
+                alt="app brand"
+                width={100}
+                height={50}
+                className="h-10 w-10"
+              />
             </Link>
           </div>
 
@@ -39,32 +44,44 @@ const HeaderNav = async ({  }: Props) => {
 
           <ToggleMenuNav />
 
-          <ul className='hidden md:flex gap-4'>
-            <li className='text-white font-bold relative' title={"Notification"}>
-              <CartBadge type="notification"/>
+          <ul className="hidden md:flex gap-4">
+            <li
+              className="text-white font-bold relative"
+              title={'Notification'}
+            >
+              <CartBadge type="notification" />
               <Dropdown
                 menuTrigger={<Bell size={25} />}
-                menuContent={
-                  Array(4).fill("notification id").map((item, idx) => (
-                    <li key={idx} className='text-sm list-none' title="Notification">
-                      <Link href={`/notification/${item.id}`}>Notification</Link>
+                menuContent={Array(4)
+                  .fill('notification id')
+                  .map((item, idx) => (
+                    <li
+                      key={idx}
+                      className="text-sm list-none"
+                      title="Notification"
+                    >
+                      <Link href={`/notification/${item.id}`}>
+                        Notification
+                      </Link>
                     </li>
-                  ))
-                }
+                  ))}
               />
             </li>
-              
+
             <li>
               <DialogCart
                 sheetHeader={
-                  <div className='bg-primary text-white flex gap-4 p-6 py-8 font-semibold'>
+                  <div className="bg-primary text-white flex gap-4 p-6 py-8 font-semibold">
                     <ShoppingCartIcon size={24} /> <span>Shopping Cart</span>
                   </div>
                 }
                 sheetTrigger={
-                  <div className='flex gap-2 relative cursor-pointer'>
-                    <CartBadge type="cart"/>
-                    <ShoppingBasket size={25}/> <span className='md:hidden font-semibold'>Shopping Cart</span>
+                  <div className="flex gap-2 relative cursor-pointer">
+                    <CartBadge type="cart" />
+                    <ShoppingBasket size={25} />{' '}
+                    <span className="md:hidden font-semibold">
+                      Shopping Cart
+                    </span>
                   </div>
                 }
                 sheetContent={<BasketCard />}
@@ -74,30 +91,35 @@ const HeaderNav = async ({  }: Props) => {
             </li>
 
             <li>
-              <ul className=''>
-                {session?.user
-                  ? (
-                    <Dropdown
-                      menuTrigger={<User2 size={25} />}
-                      menuContent={[...list.map(({ href, Icon, title }, idx) => (
-                        <li key={idx} className='font-semibold group list-none'>
-                          <Link href={href} className='flex items-center gap-2 group-hover:text-primary'>
+              <ul className="">
+                {session?.user ? (
+                  <Dropdown
+                    menuTrigger={<User2 size={25} />}
+                    menuContent={[
+                      ...list.map(({ href, Icon, title }, idx) => (
+                        <li key={idx} className="font-semibold group list-none">
+                          <Link
+                            href={href}
+                            className="flex items-center gap-2 group-hover:text-primary"
+                          >
                             <Icon size={25} /> <span>{title}</span>
                           </Link>
                         </li>
                       )),
-                      <li key={"userauth"} className='font-semibold group list-none'>
-                        <UserAuth className='flex items-center gap-2 group-hover:text-primary' />
+                      <li
+                        key={'userauth'}
+                        className="font-semibold group list-none"
+                      >
+                        <UserAuth className="flex items-center gap-2 group-hover:text-primary" />
                       </li>,
-                      ]}
-                      className='w-[14rem]'
-                    />
-                  )
-                  : (
+                    ]}
+                    className="w-[14rem]"
+                  />
+                ) : (
                   <DialogPopup
                     dialogTrigger={<User2 size={25} />}
                     dialogTitle="User Authentication"
-                    className='w-96 md:w-1/2 lg:w-1/3'
+                    className="w-96 md:w-1/2 lg:w-1/3"
                     dialogContent={<Signin />}
                   />
                 )}
@@ -107,12 +129,12 @@ const HeaderNav = async ({  }: Props) => {
         </nav>
       </MotionSlide>
 
-      <div className='hidden md:flex scroll-cat mx-4 md:mx-32 items-center gap-2 overflow-x-auto text-sm whitespace-nowrap'>
-        {
-          Categories.filter((cat) => cat.parentId == '').map((cat, idx) => (
-            <Link href={`/category/${cat.id}`} key={cat.name} className='p-4'>{cat.name}</Link>
-          ))
-        }
+      <div className="hidden md:flex scroll-cat mx-4 md:mx-32 items-center gap-2 overflow-x-auto text-sm whitespace-nowrap">
+        {Categories.filter((cat) => cat.parentId == '').map((cat, idx) => (
+          <Link href={`/category/${cat.id}`} key={cat.name} className="p-4">
+            {cat.name}
+          </Link>
+        ))}
       </div>
     </header>
   );
